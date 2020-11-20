d8b75f2569b53bf013121a5015e74ab8c0c97f0c
Alfredo Picazo <apicazojr@gmail.com>
2020-11-19 23:33:59 -0800

    Implemented main landing page animations.

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,8 +1,87 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import useWindowDimensions from './WindowSize';
+import {useTransition, animated, useChain} from "react-spring";
 const Landing = () => {
   // approach using react hooks
   const { height, width } = useWindowDimensions();
+  // considering
+  const [show, set] = useState(false);
+
+  const dateTransRef = useRef()
+  const [showDate, setShowDate] = useState(false)
+  const dateTransitions = useTransition(showDate, null, {
+    ref: dateTransRef,
+    from: { opacity: 0, transform: 'translateY(-100%)' },
+    enter: { opacity: 1, transform: 'translateY(0)' },
+    leave: { opacity: 0, transform: 'translateY(-100%)' },
+  })
+  const groomTransRef = useRef()
+  const [showGroom, setShowGroom] = useState(false)
+  const groomTransitions = useTransition(showGroom, null, {
+    ref: groomTransRef,
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  })
+  const brideTransRef = useRef()
+  const [showBride, setShowBride] = useState(false)
+  const brideTransitions = useTransition(showBride, null, {
+    ref: brideTransRef,
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  })
+
+  // const centerPieceTransRef = useRef()
+  // const [showCenterPiece, setShowCenterPiece] = useState(true)
+  // const centerPieceTransitions = useTransition(showCenterPiece, null, {
+  //   ref: centerPieceTransRef,
+  //   from: { opacity: 0 },
+  //   enter: { opacity: 1 },
+  //   leave: { opacity: 0 },
+  // })
+  const heartTransRef = useRef()
+  const [showHeart, setShowHeart] = useState(true)
+  const heartTransitions = useTransition(showHeart, null, {
+    ref: heartTransRef,
+    from: { opacity: 1 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  })
+  const flowerTransRef = useRef()
+  const [transitionFlower, setTransitionFlower] = useState(true)
+  const flowerTransitions = useTransition(transitionFlower, null, {
+    from: { opacity: 0, transform: 'scale(.2) rotate(0deg)', transition: 'all .4s ease-in-out' },
+    enter: { opacity: 1, transform: 'scale(.8) rotate(45deg)', transition: 'all .4s ease-in-out' },
+    leave: { opacity: 0 },
+  })
+
+  const announcementTransRef = useRef()
+  const [showAnnouncement, setShowAnnouncement] = useState(false)
+  const announcementTransitions = useTransition(showAnnouncement, null, {
+    ref: announcementTransRef,
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  })
+
+  useChain([dateTransRef, groomTransRef, brideTransRef, heartTransRef, flowerTransRef, announcementTransRef] , [0, 0.5, 0.5, 1, 1.5, 2])
+
+
+
+
+
+
+  if (!showDate) setShowDate(true);
+  if (!showGroom) setShowGroom(true);
+  if (!showBride) setShowBride(true);
+  //if (showCenterPiece) setShowCenterPiece(true);
+  if (showHeart) setShowHeart(false);
+  if (!transitionFlower) setTransitionFlower(true);
+  if (!showAnnouncement) setShowAnnouncement(true);
+
+
+
   return (
     <section
       style={{height: height}}
@@ -12,23 +91,75 @@ const Landing = () => {
         <div className="wrapper">
           <div className='flex-grid'>
             <div className="col-3">
-              <h1>Alfredo</h1>
+              { groomTransitions.map(({ item, key, props }) =>
+                item &&
+                <animated.h1
+                  key={key}
+                  style={props}
+                >Alfredo</animated.h1>
+              ) }
             </div>
             <div className="col-3">
               <div id="wedding-piece">
-                <div>
-                  <img src="/images/CenterHeart.png" id="center-heart" className="img-responsive" alt="Center Piece" />
-                  <img src="/images/CenterFlower.png" id="center-flower" className="img-responsive" alt="Center Piece" />
-                </div>
+                  { heartTransitions.map(({ item, key, props }) =>
+                    item &&
+                    <animated.img
+                      key={key}
+                      style={props}
+                      src="/images/CenterHeart.png"
+                      id="center-heart"
+                      className="img-responsive"
+                      alt="Center Piece"
+                    ></animated.img>
+                  ) }
+                  { flowerTransitions.map(({ item, key, props }) =>
+                    item &&
+                    <animated.img
+                      key={key}
+                      style={props}
+                      src="/images/CenterFlower.png"
+                      id="center-flower"
+                      className="img-responsive"
+                      alt="Center Piece"
+                    ></animated.img>
+                  ) }
               </div>
             </div>
-            <div className="col-3"><h1>Joanna</h1></div>
+            <div className="col-3">
+              { brideTransitions.map(({ item, key, props }) =>
+                item &&
+                <animated.h1
+                  key={key}
+                  style={props}
+                >Joanna</animated.h1>
+              ) }
+            </div>
           </div>
           <div className='flex-grid'>
-            <div className="col-1"><h2 id="wDate" style={{opacity: 1}}>April 25, 2015</h2></div>
+            <div className="col-1">
+              { dateTransitions.map(({ item, key, props }) =>
+                item &&
+                <animated.h2
+                  key={key}
+                  style={props}
+                >
+                  April 25, 2015
+                </animated.h2>
+              ) }
+            </div>
           </div>
           <div className='flex-grid'>
-            <div className="col-1"><h3 id="wMsg" style={{opacity: 1}}>ARE GETTING MARRIED</h3></div>
+            <div className="col-1">
+              { announcementTransitions.map(({ item, key, props }) =>
+                item &&
+                <animated.h3
+                  key={key}
+                  style={props}
+                >
+                  ARE GETTING MARRIED
+                </animated.h3>
+              ) }
+            </div>
           </div>
         </div>
       </div>
